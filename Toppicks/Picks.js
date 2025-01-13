e06918a4320d08584632a6c53393e7a71f2fb793
jonsi <jonsimisharwal@gmail.com>
2025-01-13 21:47:00 +0530

    top picks

---
+++ Toppicks/Picks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Picks(){
+    return (
+        <div className='Picks'>
+            <p>Top Picks For You</p>
+            <div className=''>
+
+            </div>
+        </div>
+    );
+}
