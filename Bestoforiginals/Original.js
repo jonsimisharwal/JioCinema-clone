4b2122632b82bfa355fca172b88f27d7969245a1
jonsi <jonsimisharwal@gmail.com>
2025-01-14 23:50:48 +0530

    Original

---
+++ Bestoforiginals/Original.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Illegal from './illegal.avif'
+import Khalbali from './khalbali.avif'
+import doctors from './doctors.avif'
+import dus from './dus.avif'
+import ishq from './ishq.avif'
+import cadets from './cadets.avif'
+import bunty from './bunty.avif'
+import gaanth from './gaanth.avif'
+import pill from './pill.avif'
+import shekhar from './shekhar.avif'
+import taali from './taali.avif'
+import Magic from './magic.avif'
+import Mahim from './Mahim.avif'
+import Motu from './motu.avif'
+import Moonwalk from './moonwalk.avif'
+import './original.css'
+import Odata from './Odata.js'
+function Original(){
+    <div className='original'>
+     <p>Best OF Original</p>
+     <div>
+        <Odata image={taali} />
+        <Odata image={Mahim} />
+        <Odata image={Motu} />
+        <Odata image={Moonwalk} />
+        <Odata image={Magic} />
+        <Odata image={Khalbali} />
+        <Odata image={ishq} />
+        <Odata image={Illegal} />
+        <Odata image={gaanth} />
+        <Odata image={dus} />
+        <Odata image={doctors} />
+        <Odata image={cadets} />
+        <Odata image={bunty} />
+        <Odata image={pill} />
+        <Odata image={shekhar} />
+        <Odata image={taali} />
+     </div>
+    </div>
+}
